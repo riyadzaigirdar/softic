7e5ff347440ee29d014028366329213b27641a00
riyad <zaigirdar@loopfreight.io>
2022-11-26 19:38:21 +0600

    initialized modules, services, controllers

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
+import { PostModule } from './modules/post/post.module';
+import { CommentModule } from './modules/comment/comment.module';
+
+@Module({
+  imports: [UserModule, PostModule, CommentModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
