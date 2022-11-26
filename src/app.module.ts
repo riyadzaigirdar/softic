056c97ccc963629630c20add5341d8c40da28324
riyad <zaigirdar@loopfreight.io>
2022-11-26 19:40:32 +0600

    clean src folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
 import { PostModule } from './modules/post/post.module';
 import { CommentModule } from './modules/comment/comment.module';
@@ -8,6 +7,6 @@ import { CommentModule } from './modules/comment/comment.module';
 @Module({
   imports: [UserModule, PostModule, CommentModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
